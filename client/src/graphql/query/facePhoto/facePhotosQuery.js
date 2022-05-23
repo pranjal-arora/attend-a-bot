413fce6ace2c83c99618b0a038d485332af101d7
pranjal-arora <prv.gma@gmail.com>
2022-05-23 21:03:00 +0530

    commit 8 - graphql query

---
+++ client/src/graphql/query/facePhoto/facePhotosQuery.js
@@ -0,0 +1,50 @@
+import gql from 'graphql-tag';
+
+export const FETCH_FACE_PHOTOS_COUNT_QUERY = gql`
+  query getFacePhotosCount {
+    getFacePhotosCount
+  }
+`;
+
+
+export const FETCH_FACE_PHOTOS_QUERY = gql`
+  query getFacePhotos($cursor: ID, $limit: Int!) {
+    getFacePhotos(cursor: $cursor, limit: $limit) {
+      facePhotos {
+        _id
+        faceDescriptor
+        photoURL
+        createdAt
+      }
+      hasNextPage
+    }
+  }
+`;
+
+// fetch the face dataset of all the participant in the course from the server using the query.
+export const FETCH_FACE_MATCHER_IN_COURSE_QUERY = gql`
+  query getFaceMatcherInCourse($courseID: String!) {
+    getFaceMatcherInCourse(courseID: $courseID) {
+      course {
+        _id
+        code
+        name
+        session
+        shortID
+      }
+      matcher {
+        student {
+          _id
+          firstName
+          lastName
+          cardID
+          profilePictureURL
+        }
+        facePhotos {
+          faceDescriptor
+          photoURL
+        }
+      }
+    }
+  }
+`;
