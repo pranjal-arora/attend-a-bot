0ff87623f514bad43aeada3954dc3a7cb27b085b
pranjal-arora <prv.gma@gmail.com>
2022-05-29 07:30:55 +0530

    commit 19 - server

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import {
+  AttendanceProvider,
+  AuthProvider,
+  CourseProvider,
+  EnrolmentProvider,
+  FacePhotoProvider,
+  FaceThresholdDistanceProvider,
+  NavbarProvider,
+  NotificationProvider,
+} from "./context";
+import {
+  CourseDetails,
+  Dashboard,
+  MainMenu,
+  NoFound,
+  Notifications,
+  Profile,
+  SignIn,
+  SignUp,
+  InCourseAttendanceHistory,
+  SingleAttendanceHistory,
+  AttendanceRoom,
+  UndefinedCardIDAndRole,
+  PrivacyPolicy,
+  TermCondition,
+  UserGuidelines,
+} from "./pages/common";
+import {
+  AttendanceForm,
+} from "./pages/lecturerPage";
+import { FaceGallery } from "./pages/studentPage";
+import {
+  AuthRoute,
+  LecturerRoute,
+  ProtectedRoute,
+  StudentRoute,
+  UndefinedCardIDAndRoleRoute,
+} from "./routes";
+import Testing from "./Testing";
+import "lazysizes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavbarProvider>
+      <AuthProvider>
+        <NotificationProvider>
+          <AttendanceProvider>
+            <CourseProvider>
+              <EnrolmentProvider>
+                <FacePhotoProvider>
+                  <FaceThresholdDistanceProvider>
+                    <Router>
+                      <Switch>
+                        <ProtectedRoute exact path="/" component={MainMenu} />
+                        <ProtectedRoute
+                          exact
+                          path="/signin"
+                          component={SignIn}
+                        />
+                        <ProtectedRoute
+                          exact
+                          path="/signup"
+                          component={SignUp}
+                        />
+
+                        <UndefinedCardIDAndRoleRoute
+                          exact
+                          path="/aboutYourself"
+                          component={UndefinedCardIDAndRole}
+                        />
+                        <AuthRoute
+                          exact
+                          path="/dashboard"
+                          component={Dashboard}
+                        />
+                        <AuthRoute exact path="/profile" component={Profile} />
+                        <AuthRoute
+                          exact
+                          path="/notification"
+                          component={Notifications}
+                        />
+                        <AuthRoute
+                          exact
+                          path="/course/:id"
+                          component={CourseDetails}
+                        />
+                        {/* <AuthRoute exact path='/testing' component={Testing} /> */}
+                        <LecturerRoute
+                          exact
+                          path="/course/:id/attendanceForm"
+                          component={AttendanceForm}
+                        />
+                        <AuthRoute
+                          exact
+                          path="/course/:courseID/attendanceRoom/:attendanceID"
+                          component={AttendanceRoom}
+                        />
+
+                        <AuthRoute
+                          exact
+                          path="/course/:id/attendanceList"
+                          component={InCourseAttendanceHistory}
+                        />
+
+                        <AuthRoute
+                          exact
+                          path="/course/:courseID/attendanceList/:attendanceID"
+                          component={SingleAttendanceHistory}
+                        />
+
+                        <StudentRoute
+                          exact
+                          path="/facegallery"
+                          component={FaceGallery}
+                        />
+                        <Route
+                          exact
+                          path="/termandcondition"
+                          component={TermCondition}
+                        />
+                        <Route
+                          exact
+                          path="/privacypolicy"
+                          component={PrivacyPolicy}
+                        />
+                        <Route
+                          exact
+                          path="/userguidelines"
+                          component={UserGuidelines}
+                        />
+                        <AuthRoute component={NoFound} />
+                      </Switch>
+                    </Router>
+                  </FaceThresholdDistanceProvider>
+                </FacePhotoProvider>
+              </EnrolmentProvider>
+            </CourseProvider>
+          </AttendanceProvider>
+        </NotificationProvider>
+      </AuthProvider>
+    </NavbarProvider>
   );
 }
 
